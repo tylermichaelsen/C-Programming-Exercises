306c87c8018b7133add3742dd14239fec1dd0646
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 04:01:37 -0500

    Find the greatest number of the three and output it

--- 2_6.cpp
+++ 2_6.cpp
@@ -6,4 +6,10 @@ int main() {
     std::cout << "Please enter three integer numbers (seperated by spaces): ";
     int n0 = 0, n1 = 0, n2 = 0;
     std::cin >> n0 >> n1 >> n2;
+    if(n0 > n1 && n0 > n2)
+        std::cout << n0 << ", ";
+    if(n1 > n0 && n1 > n2)
+        std::cout << n1 << ", ";
+    if(n2 > n0 && n2 > n1)
+        std::cout << n2 << ", ";
 }
