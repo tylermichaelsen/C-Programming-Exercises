ce177fd46a5068fdd5c87ea0f0e29d52864922b0
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 04:04:16 -0500

    Fix readibility

--- 2_6.cpp
+++ 2_6.cpp
@@ -4,8 +4,12 @@
 
 int main() {
     std::cout << "Please enter three integer numbers (seperated by spaces): ";
+
     int n0 = 0, n1 = 0, n2 = 0;
+
     std::cin >> n0 >> n1 >> n2;
+
+    // Find first number of sequence and output it
     if(n0 > n1 && n0 > n2)
         std::cout << n0 << ", ";
     if(n1 > n0 && n1 > n2)
