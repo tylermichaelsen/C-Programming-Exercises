49ba25c222bfd46299fe264cf1851f063c17202d
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 04:11:14 -0500

    Fix case where middle number was not properly identified

--- 2_6.cpp
+++ 2_6.cpp
@@ -18,10 +18,10 @@ int main() {
         std::cout << n2 << ", ";
 
     // Find second number of sequence and output it
-    if(n0 < n1 && n0 > n2)
+    if(n0 < n1 && n0 > n2 || n0 > n1 && n0 < n2)
         std::cout << n0 << ", ";
-    if(n1 < n0 && n1 > n2)
+    if(n1 < n0 && n1 > n2 || n1 > n0 && n1 < n2)
         std::cout << n1 << ", ";
-    if(n2 < n0 && n2 > n1)
+    if(n2 < n0 && n2 > n1 || n2 > n0 && n2 < n1)
         std::cout << n2 << ", ";
 }
