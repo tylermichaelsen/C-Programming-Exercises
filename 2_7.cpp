96a17882187b6fcebdef6ae2f4d4c2ef0d7c7cd7
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 09:04:09 -0500

    Check if the strings are already sorted and print if so

--- 2_7.cpp
+++ 2_7.cpp
@@ -6,4 +6,8 @@ int main() {
     std::cout << "Enter three names seperated by a space: ";
     std::string s0, s1, s2;
     std::cin >> s0 >> s1 >> s2;
+
+    // Check if its already sorted alphabetically
+    if(s0 < s1 && s1 < s2)
+        std::cout << s0 << ", " << s1 << ", " << s2;
 }
