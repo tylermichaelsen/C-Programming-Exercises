8fb62f046a5266c8d1eab9e8a74e12dc8902915d
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 03:50:44 -0500

    Modify 2_4.cpp code to use doubles instead of integers

---
+++ 2_5.cpp
@@ -0,0 +1,33 @@
+// Number comparison using doubles
+
+#include <iostream>
+
+int main() {
+    double val1 = 0, val2 = 0;
+    std::cout << "Please enter two double-floating point numbers seperated by a space: ";
+    std::cin >> val1 >> val2;
+
+    // Find the larger number
+    if(val1 > val2)
+        std::cout << val1 << " is larger than " << val2 << '\n';
+    if(val2 > val1)
+        std::cout << val2 << " is larger than " << val1 << '\n';
+
+    // Find the smaller number
+    if(val1 < val2)
+        std::cout << val1 << " is less than " << val2 << '\n';
+    if(val2 < val1)
+        std::cout << val2 << " is less than " << val1 << '\n';
+
+    // Find the sum
+    std::cout << val1 << " + " << val2 << " = " << val1+val2 << '\n';
+
+    // Find the difference
+    std::cout << val1 << " - " << val2 << " = " << val1-val2 << '\n';
+
+    // Find the product
+    std::cout << val1 << " * " << val2 << " = " << val1*val2 << '\n';
+
+    // Find the ratio
+    std::cout << val1 << " / " << val2 << " = " << val1/val2 << '\n';
+}
