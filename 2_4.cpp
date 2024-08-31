43d5dd8e8796ec7abb1f083aaee5a09a10ce08e6
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 03:27:45 -0500

    Find the larger number of the two integer values given

--- 2_4.cpp
+++ 2_4.cpp
@@ -6,4 +6,10 @@ int main() {
     int val1 = 0, val2 = 0;
     std::cout << "Please enter two integer numbers seperated by a space: ";
     std::cin >> val1 >> val2;
+
+    // Find the larger number
+    if(val1 > val2)
+        std::cout << val1 << " is larger than " << val2 << '\n';
+    if(val2 > val1)
+        std::cout << val2 << " is larger than " << val1 << '\n';
 }
