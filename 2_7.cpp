c5a055ef529fd465064384e092e9d189cb0b20d8
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-08-31 05:47:55 -0500

    Modify exercise 6 code

---
+++ 2_7.cpp
@@ -0,0 +1,35 @@
+// Output sequential strings
+
+#include <iostream>
+
+int main() {
+    std::cout << "Please enter three names (seperated by spaces): ";
+
+    std::string n0 = 0, n1 = 0, n2 = 0;
+
+    std::cin >> n0 >> n1 >> n2;
+
+    // Find first name of sequence and output it
+    if(n0 > n1 && n0 > n2)
+        std::cout << n0 << ", ";
+    if(n1 > n0 && n1 > n2)
+        std::cout << n1 << ", ";
+    if(n2 > n0 && n2 > n1)
+        std::cout << n2 << ", ";
+
+    // Find second name of sequence and output it
+    if(n0 < n1 && n0 > n2 || n0 > n1 && n0 < n2)
+        std::cout << n0 << ", ";
+    if(n1 < n0 && n1 > n2 || n1 > n0 && n1 < n2)
+        std::cout << n1 << ", ";
+    if(n2 < n0 && n2 > n1 || n2 > n0 && n2 < n1)
+        std::cout << n2 << ", ";
+    
+    // Find third name of sequence and output it
+    if(n0 < n1 && n0 < n2)
+        std::cout << n0;
+    if(n1 < n0 && n1 < n2)
+        std::cout << n1;
+    if(n2 < n0 && n2 < n1)
+        std::cout << n2;
+}
