f8cfe247c9174db906260a36feabd9d279137b1d
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-01 14:11:51 -0500

    Create a variable and store the users input in it

--- 2_8.cpp
+++ 2_8.cpp
@@ -5,4 +5,8 @@
 int main(){
     // Prompt the user to enter a number
     std::cout << "Please enter a number to figure out if it is even or odd: ";
+
+    // Create an integer variable and store the users input in it
+    int number = 0;
+    std::cin >> number;
 }
