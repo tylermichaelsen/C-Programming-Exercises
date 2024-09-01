55a99e4e72139b8cafd58ddc6d9ecff4043b9860
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-01 14:22:13 -0500

    Create a string variable and store users input in it

--- 2_9.cpp
+++ 2_9.cpp
@@ -5,4 +5,8 @@
 int main() {
     // Prompt the user to give us a number in the form of a word
     std::cout << "Please spell out a number: ";
+
+    // Create a string variable for users input
+    std::string word;
+    std::cin >> word;
 }
