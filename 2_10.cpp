f444faa9ce845edc62aa4740d9769e7abb2d7999
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-02 14:57:21 -0500

    Prompt the user

--- 2_10.cpp
+++ 2_10.cpp
@@ -6,4 +6,7 @@ int main() {
     // Create input variables
     char operation;
     double num1 = 0, num2 = 0;
+
+    // Prompt the user
+    std::cout << "Please enter a operation character followed by 2 numbers: ";
 }
