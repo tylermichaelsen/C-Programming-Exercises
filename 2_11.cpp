33ea267e7dfc2824d20fb48f609b95b7ccfeebd0
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-03 13:28:45 -0500

    Prompt user for nickels

--- 2_11.cpp
+++ 2_11.cpp
@@ -7,6 +7,10 @@ int main() {
     int pennies, nickels, dimes, quarters, halfDollars;
 
     // Ask and store pennies
-    std::cout << "How many pennies do you have?";
+    std::cout << "How many pennies do you have?\n";
     std::cin >> pennies;
+
+    // Ask and store nickels
+    std::cout << "How many nickels do you have?\n";
+    std::cin >> nickels;
 }
