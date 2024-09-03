26c54ec3493ea82bdedc42b249d0bb7947a0a99b
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-03 13:27:26 -0500

    Prompt user for pennys

--- 2_11.cpp
+++ 2_11.cpp
@@ -5,5 +5,8 @@
 int main() {
     // Create variables for each type of coin
     int pennies, nickels, dimes, quarters, halfDollars;
-    
+
+    // Ask and store pennies
+    std::cout << "How many pennies do you have?";
+    std::cin >> pennies;
 }
