584f55fa8591fb61406bd2d16ab1bf873af578c5
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-04 12:46:33 -0500

    Modify if statements single switch statement

--- try_this.cpp
+++ try_this.cpp
@@ -12,12 +12,27 @@ int main() {
     std::cout << "Please enter y k or p and then a number: ";
     std::cin >> currency >> money;
 
-    if(currency=='y')
+    /* if(currency=='y')
         std::cout << money * yen;
     else if(currency=='k')
         std::cout << money * kroner;
     else if(currency=='p')
         std::cout << money * pounds;
     else 
-        std::cout << "Not a valid currency character";
+        std::cout << "Not a valid currency character" */
+
+    switch(currency) {
+        case 'y':
+            std::cout << money * yen;
+            break;
+        case 'k':
+            std::cout << money * kroner;
+            break;
+        case 'p':
+            std::cout << money * pounds;
+            break;
+        default:
+            std::cout << "Not a valid currency character";
+            break;
+    }
 }
