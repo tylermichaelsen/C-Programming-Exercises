eee3474b87a6e5cd6b754fcd5a7748bd383dbe1c
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-06 18:55:46 -0500

    Output the larger and smaller number

--- drill.cpp
+++ drill.cpp
@@ -5,7 +5,15 @@
 
 int main() {
     for(int x, y; std::cin>>x>>y;) {
-        std::cout << x << '\t' << y << '\n';
+        if(x < y) {
+            std::cout << "the smaller value is: " << x << '\n';
+            std::cout << "the larger value is: " << y << '\n';
+        } else if(y < x) {
+            std::cout << "the smaller value is: " << y << '\n';
+            std::cout << "the larger value is: " << x << '\n';
+        }
+            
+
         if(x == '|' || y == '|')
             break;
     }
