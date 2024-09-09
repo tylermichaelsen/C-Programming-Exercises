5fc4bcd477605245fea8c888e4e2f43e9e0d06b6
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-09 04:26:59 -0500

    Delete loop body for reproduction

--- drill.cpp
+++ drill.cpp
@@ -5,26 +5,8 @@
 
 int main() {
     double difference;
-    for(double x, y; std::cin>>x>>y;) {
-        if(x < y) {
-            std::cout << "the smaller value is: " << x << '\n';
-            std::cout << "the larger value is: " << y << '\n';
-            difference = y - x;
-        } else if(y < x) {
-            std::cout << "the smaller value is: " << y << '\n';
-            std::cout << "the larger value is: " << x << '\n';
-            difference = x - y;
-        } else if(x == y) {
-            std::cout << "the numbers are equal" << '\n';
-        }
+    for(double number; std::cin>>number;) {
 
-        if(difference <= 1.0/100 && difference > 0
-        || difference >= -1.0/100 && difference < 0) {
-            std::cout << "the numbers are almost equal" << '\n';
-        }
-
-        if(x == '|' || y == '|')
-            break;
     }
         
 }
