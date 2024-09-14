ae72b63834615cfc20a882935f8de7f8bb93d821
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-14 00:39:34 -0500

    Rewrite program 3.6.3

--- 3_3.cpp
+++ 3_3.cpp
@@ -1,8 +1,21 @@
 // Fix 3.6.3 program to properly display median
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 int main() {
-    
+    std::vector<double> temps;
+    for(double temp; std::cin>>temp;)
+        temps.push_back(temp);
+
+    // compute mean temperature:
+    double sum = 0;
+    for (double x : temps)
+        sum += x;
+    std::cout << "Average temperature: " << sum/temps.size() << '\n';
+
+    // comuter median temperature
+    std::ranges::sort(temps);
+    std::cout << "Median temperature: " << temps[temps.size()/2] << '\n';
 }
