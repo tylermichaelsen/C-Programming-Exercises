16863a6d4c324ba2ff5ad93ea730df3fd0ed683f
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-14 15:31:23 -0500

    Prompt the user to think of a number

--- 3_5.cpp
+++ 3_5.cpp
@@ -4,4 +4,6 @@
 
 int main() {
     int guess = 0;
+    std::cout << "Think of a number between 1 and 100, and then answer the following questions by writing a 'y' for yes or a 'n' for no.\n";
+    
 }
