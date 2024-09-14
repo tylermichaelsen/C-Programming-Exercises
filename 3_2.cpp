c618e22c4646d45127b4eaf4c8872ee91b3cf9d4
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-13 20:26:17 -0500

    Initial commit

---
+++ 3_2.cpp
@@ -0,0 +1,7 @@
+// Write a program that reads a string from input and then, for each character read, prints out the character and its integer value on a line.
+
+#include <iostream>
+
+int main() {
+    
+}
