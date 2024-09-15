cd8016ee7f01815a30651c1966d0e88af0e2c450
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-14 19:40:10 -0500

    Move prompt

--- 3_6.cpp
+++ 3_6.cpp
@@ -6,9 +6,9 @@ int main() {
     double num1 = 0, num2 = 0;
     char operation = ' ';
 
-    while(std::cin>>num1>>num2>>operation) {
+    std::cout << "Please enter 2 numbers followed by an operator character: ";
 
-        std::cout << "Please enter 2 numbers followed by an operator character: ";
+    while(std::cin>>num1>>num2>>operation) {
 
         switch(operation) {
             case '+':
