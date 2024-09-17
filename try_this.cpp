e9e72d4751c57df68866ab7f5b0486f8e0f40558
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-17 14:16:46 -0500

    Finish try this code for further testing

--- try_this.cpp
+++ try_this.cpp
@@ -4,10 +4,28 @@
 
 // Compiler Error testing
 
+
+
+int framed_area(int x, int y) {
+    constexpr int frame_width = 2;
+    if(x-frame_width<=0 || y-frame_width <= 0)
+        std::cout << "Error\n";
+    return area(x-frame_width, y-frame_width);
+}
+
 int area(int length, int width) {
     if(length<=0 || width<=0)
         return -1;
     return length*width;
+}
+
+int f(int x, int y, int z) {
+    int area1 = area(x,y);
+    if(area<=0)
+        std::cout << "Error\n";
+    int area2 = framed_area(1,z);
+    int area3 = framed_area(y,z);
+    double ratio = double(area1)/area3;
 }
 
 int main () {
