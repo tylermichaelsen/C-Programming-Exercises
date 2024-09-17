2562b1fc4aeca5e8065d5643e5946181c4900c63
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-17 13:47:18 -0500

    Delete for new exercise

--- try_this.cpp
+++ try_this.cpp
@@ -1,25 +1,9 @@
 // This gets updated to the newest 'Try this' exercise I've worked on
 
 #include <iostream>
-#include <vector>
 
-// Bleep unwanted input
+// Compiler Error testing
 
 int main() {
 
-    std::vector<std::string> disliked = {"Broccoli", "think", "bright"};
-
-    std::vector<std::string> words; 
-
-    for (std::string word; std::cin>>word; ) 
-        words.push_back(word);
-
-    for(int i = 0; i<words.size(); ++i) {
-        for(int j = 0; j<disliked.size(); ++j) {
-            if(words[i] == disliked[j]) {
-                words[i] = "BLEEP";
-            }
-        }
-        std::cout << words[i] << '\n';
-    }
 }
