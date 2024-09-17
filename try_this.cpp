2a02b855b0dc16a0df9639aff7594c286c328027
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-17 14:10:28 -0500

    create area function

--- try_this.cpp
+++ try_this.cpp
@@ -5,9 +5,11 @@
 // Compiler Error testing
 
 int area(int length, int width) {
-    return length * width;
+    if(length<=0 || width<=0)
+        return -1;
+    return length*width;
 }
 
-int main() {
+int main () {
 
 }
