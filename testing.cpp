87bc67f9dd70aa428612c0ce4b2d189686ce2203
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-19 06:58:32 -0500

    Copy program from book

--- testing.cpp
+++ testing.cpp
@@ -1,3 +1,13 @@
 // This is where I do some testing for concepts and 
 // ideas that I need more work on
 
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> v;
+    for(int x; std::cin>>x;)
+        v.push_back(x);
+    for(int i = 0; i <= v.size(); ++i)
+        std::cout << "v[" << i << "] == " << v[i] << '\n';
+}
