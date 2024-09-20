0187cbd7be94bf583e945fe24c8193126e3d91bd
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-20 11:31:19 -0500

    Finish

--- testing.cpp
+++ testing.cpp
@@ -5,9 +5,21 @@
 #include <vector>
 
 int main() {
-    std::vector<int> v;
-    for(int x; std::cin>>x;)
-        v.push_back(x);
-    for(int i = 0; i <= v.size(); ++i)
-        std::cout << "v[" << i << "] == " << v[i] << '\n';
+    try {
+        std::vector<int> v;
+        for(int x; std::cin>>x;)
+            v.push_back(x);
+        for(int i = 0; i <= v.size(); ++i)
+            std::cout << "v[" << i << "] == " << v[i] << '\n';
+        }
+
+    catch(std::out_of_range) {
+        std::cerr << "Oops! Range error\n";
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Exception: something went wrong\n";
+        return 2;
+    }
 }
+
