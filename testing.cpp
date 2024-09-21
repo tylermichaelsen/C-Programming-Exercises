f0ece9f92dadaee08a04095ce04906b41b341c12
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-20 19:34:24 -0500

    Clear for more testing with error handling

--- testing.cpp
+++ testing.cpp
@@ -2,24 +2,8 @@
 // ideas that I need more work on
 
 #include <iostream>
-#include <vector>
 
 int main() {
-    try {
-        std::vector<int> v;
-        for(int x; std::cin>>x;)
-            v.push_back(x);
-        for(int i = 0; i <= v.size(); ++i)
-            std::cout << "v[" << i << "] == " << v[i] << '\n';
-        }
-
-    catch(std::out_of_range) {
-        std::cerr << "Oops! Range error\n";
-        return 1;
-    }
-    catch(...) {
-        std::cerr << "Exception: something went wrong\n";
-        return 2;
-    }
+    
 }
 
