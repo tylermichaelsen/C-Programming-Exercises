c03ae386d1abbe94f8ca934cf5f627d4c3128822
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-20 19:41:20 -0500

    Create square function

--- testing.cpp
+++ testing.cpp
@@ -7,6 +7,13 @@ void error(std::string s) {
     throw std::runtime_error{s};
 }
 
+double square(double x) {
+    if(x < 0) 
+        error("Non-positive number.");
+    return x * x;
+    
+}
+
 int main() {
 
 }
